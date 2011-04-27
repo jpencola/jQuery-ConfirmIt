631f30b93a114719317152d9693300edcf308e15
jpencola <paulpencola@gmail.com>
2011-04-27 03:24:22 -0400

    Stubbing out the plugin JS per the jQuery recommendations for plugin development. I changed some of the initial assumptions and added some concepts to the mix. The underlying structure is there, but there is not a complete flow yet at the point of the confirmation (yes/no) response. Building out the links example.

---
+++ src/jWarning.js
@@ -0,0 +1,139 @@
+//TODO: remove this later...
+function trace(msg){try{console.log(msg)}catch(ex){}};
+
+/****************************************************
+POST, LINK AND REDIRECT FUNCTIONS
+****************************************************/
+
+var clickedInput;
+function initConfirm() {
+	/*
+	TO ADD CONFIRM CLICK ACTION FOR FORMS:
+	give any input submit button the class "confirm" 
+	give the button a 'title' attribute with the confirmation message (ToolTip for asp controls)
+	*/
+	$("input,button").click(function (event) {
+		clickedInput = this;
+		
+	});
+
+	$("form").submit(function () {
+
+	    trace("form submitted");
+
+	    var confirmClick = $(clickedInput).hasClass("confirm");
+	    if (confirmClick) {
+	        var msg = $(clickedInput).attr("title");
+	        if (!msg)
+	            msg = "Are you sure you wish to perform this action?";
+	        return confirm(msg);
+	    }
+
+	    return true;
+	});
+  
+	$("a").click(function (event) {
+	
+		clickedInput = this;
+		var confirmClick = $(clickedInput).hasClass("confirm");
+		if (confirmClick) {
+			var msg = $(clickedInput).attr("title");
+			if (!msg)
+				msg = "Are you sure you wish to perform this action?";
+		  
+				var answer = confirm(msg);
+				if (!answer)
+					event.preventDefault();
+		}
+		
+	});
+}
+
+
+var CONFIRM_LEAVE_SITE = "Exit Notice: The appearance of external hyperlinks does not constitute endorsement by the Federal Acquisition Institute of this Web site or the information, products, or services contained therein. For other than authorized activities such as military exchanges and MWR sites, the federal government does not exercise any editorial control over the information you may find at these locations. Such links are provided consistent with the stated purpose of this Web site.";
+function initExternalLinkWarning() {
+
+	jQuery("a").click(function (event) {
+		href = jQuery(this).attr("href");
+
+		if (!href)
+			return;
+		lowerHref = href.toLowerCase();
+
+		var isExternal = (lowerHref.indexOf("http") == 0);
+		var isMilitary = (lowerHref.indexOf(".mil") != -1);
+		var isGov = (lowerHref.indexOf(".gov") != -1);
+		var isAtrrs = (lowerHref.indexOf("atrrs") != -1);
+
+		if (isExternal && !isMilitary && !isGov && !isAtrrs) {
+		  /*  try {
+
+				jConfirm(CONFIRM_LEAVE_SITE, 'Leave Atrrs Site?', function (r) {
+					trace('Confirmed: ' + r);
+					if (r)
+						window.open(href);
+				});
+				event.preventDefault();
+			}
+			catch (e) {*/
+				var answer = confirm(CONFIRM_LEAVE_SITE);
+				if (!answer)
+					event.preventDefault();
+		   /* }*/
+		}
+	});
+}
+
+
+
+
+/*
+var formChanged = false;
+
+$(document).ready(function() {
+     $('form input[type=text].editable, form textarea.editable').each(function (i) {
+          $(this).data('initial_value', $(this).val());
+     });
+
+     $('form input[type=text].editable, form textarea.editable').keyup(function() {
+          if ($(this).val() != $(this).data('initial_value')) {
+               handleFormChanged();
+          }
+     });
+
+     $('form .editable').bind('change paste', function() {
+          handleFormChanged();
+     });
+
+     $('.navigation_link').bind("click", function () {
+          return confirmNavigation();
+     });
+});
+
+function handleFormChanged() {
+     $('#save_or_update').attr("disabled", false);
+     formChanged = true;
+}
+
+function confirmNavigation() {
+     if (formChanged) {
+          return confirm('Are you sure? Your changes will be lost!');
+     } else {
+          return true;
+     }
+}
+*/
+
+
+
+function LeaveWarning(e) {
+  var message = "It looks like you have unfinished business. Leave this page?",
+  e = e || window.event;
+  // For IE and Firefox
+  if (e) {
+    e.returnValue = message;
+  }
+
+  // For Safari
+  return message;
+};
--- src/jquery.confirm.it.js
+++ src/jquery.confirm.it.js
@@ -1,142 +1,43 @@
-
-
-
-
-
-function trace(msg){console.log(msg)}
-/****************************************************
-POST, LINK AND REDIRECT FUNCTIONS
-****************************************************/
-
-var clickedInput;
-function initConfirm() {
-	/*
-	TO ADD CONFIRM CLICK ACTION FOR FORMS:
-	give any input submit button the class "confirm" 
-	give the button a 'title' attribute with the confirmation message (ToolTip for asp controls)
-	*/
-	$("input,button").click(function (event) {
-		clickedInput = this;
+(function($){
+	$.fn.confirmIt = function(method, options){
+		//	default settings
+		var settings = {
+			selector: ".confirmit",
+			triggered_by: {'name': 'click'}
+		};
 		
-	});
-
-	$("form").submit(function () {
-
-	    trace("form submitted");
-
-	    var confirmClick = $(clickedInput).hasClass("confirm");
-	    if (confirmClick) {
-	        var msg = $(clickedInput).attr("title");
-	        if (!msg)
-	            msg = "Are you sure you wish to perform this action?";
-	        return confirm(msg);
-	    }
-
-	    return true;
-	});
-  
-	$("a").click(function (event) {
-	
-		clickedInput = this;
-		var confirmClick = $(clickedInput).hasClass("confirm");
-		if (confirmClick) {
-			var msg = $(clickedInput).attr("title");
-			if (!msg)
-				msg = "Are you sure you wish to perform this action?";
-		  
-				var answer = confirm(msg);
-				if (!answer)
-					event.preventDefault();
-		}
+		//	apply any options overrides
+		$.extend(settings, options);
 		
-	});
-}
-
-
-var CONFIRM_LEAVE_SITE = "Exit Notice: The appearance of external hyperlinks does not constitute endorsement by the Federal Acquisition Institute of this Web site or the information, products, or services contained therein. For other than authorized activities such as military exchanges and MWR sites, the federal government does not exercise any editorial control over the information you may find at these locations. Such links are provided consistent with the stated purpose of this Web site.";
-function initExternalLinkWarning() {
-
-	jQuery("a").click(function (event) {
-		href = jQuery(this).attr("href");
-
-		if (!href)
-			return;
-		lowerHref = href.toLowerCase();
-
-		var isExternal = (lowerHref.indexOf("http") == 0);
-		var isMilitary = (lowerHref.indexOf(".mil") != -1);
-		var isGov = (lowerHref.indexOf(".gov") != -1);
-		var isAtrrs = (lowerHref.indexOf("atrrs") != -1);
-
-		if (isExternal && !isMilitary && !isGov && !isAtrrs) {
-		  /*  try {
-
-				jConfirm(CONFIRM_LEAVE_SITE, 'Leave Atrrs Site?', function (r) {
-					trace('Confirmed: ' + r);
-					if (r)
-						window.open(href);
-				});
-				event.preventDefault();
+		//	for each matching confirm element...
+		//	remember any event handlers bound to it
+		//	then remove any event handlers from the element	
+		//	if the element has no event handlers then do nothing
+		$(settings.selector).each(function() {
+			var element = $(this);
+			if (element.data("events")){
+				rememberEventHandler(element);
+				element.unbind();
+				element.one(settings.triggered_by.name, onConfirm);
+			} else {
+				return;
 			}
-			catch (e) {*/
-				var answer = confirm(CONFIRM_LEAVE_SITE);
-				if (!answer)
-					event.preventDefault();
-		   /* }*/
-		}
-	});
-}
+		});
+		
+		//	the handler that intercepts the action
+		function onConfirm(){
+			var classname = $(this).attr('class');
+			var message = classname.substring(classname.indexOf("{")+1, classname.lastIndexOf("}")).split(":")[1]; // good candidate for a regexp
+			trace("confirm: "+message);
+			return false;
+		};
+		
+		//	stashes existing bound events on the element
+		function rememberEventHandler(element){
+			element.data('__deferred_event_handlers__', element.data("events"));
+		};
+	};
+})(jQuery);
 
-
-
-
-/*
-var formChanged = false;
-
-$(document).ready(function() {
-     $('form input[type=text].editable, form textarea.editable').each(function (i) {
-          $(this).data('initial_value', $(this).val());
-     });
-
-     $('form input[type=text].editable, form textarea.editable').keyup(function() {
-          if ($(this).val() != $(this).data('initial_value')) {
-               handleFormChanged();
-          }
-     });
-
-     $('form .editable').bind('change paste', function() {
-          handleFormChanged();
-     });
-
-     $('.navigation_link').bind("click", function () {
-          return confirmNavigation();
-     });
-});
-
-function handleFormChanged() {
-     $('#save_or_update').attr("disabled", false);
-     formChanged = true;
-}
-
-function confirmNavigation() {
-     if (formChanged) {
-          return confirm('Are you sure? Your changes will be lost!');
-     } else {
-          return true;
-     }
-}
-*/
-
-
-
-function LeaveWarning(e) {
-  var message = "It looks like you have unfinished business. Leave this page?",
-  e = e || window.event;
-  // For IE and Firefox
-  if (e) {
-    e.returnValue = message;
-  }
-
-  // For Safari
-  return message;
-};
+//TODO: remove this later...
+function trace(msg){try{console.log(msg)}catch(ex){}};
