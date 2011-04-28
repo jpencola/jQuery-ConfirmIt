665b1571fae00457343a0b9f87b821d0ca570c6b
jpencola <paulpencola@gmail.com>
2011-04-28 01:47:42 -0400

    First cut of the logic for unbind/bind/rebind on the target element. Added a working demo in the example file for a confirm button that adds 10 to an input field's value.

--- src/jquery.confirm.it.js
+++ src/jquery.confirm.it.js
@@ -13,31 +13,69 @@
 		//	then remove any event handlers from the element	
 		//	if the element has no event handlers then do nothing
 		$(this).each(function() {
+			var target = this;
 			var element = $(this);
 			if (element.data("events")){
-				rememberEventHandlers(element);
-				element.unbind();
-				var classname = element.attr('class');
-				var confirm_message = classname.substring(classname.indexOf("{")+1, classname.lastIndexOf("}")).split(":")[1]; // good candidate for a regexp
-				element.bind(settings.triggered_by, (function(){showConfirm(element, confirm_message)}));
+				rememberEventHandler(element);
+				removeEventHandler(element);
+				bindConfirmHandler(element);
 			} else {
 				return;
 			}
 		});
 		
+		//	stashes existing trigger event from the element
+		function rememberEventHandler(element){
+			var events = element.data("events");
+			var event_memory = [];
+			for (var event in events){
+				var event_object = events[event][0];
+				if (event_object.type == settings.triggered_by){
+					event_memory.push({
+						type: event_object.type,
+						handler: event_object.handler
+					});
+					break;
+				}
+			}
+			element.data('__deferred_event_handlers__', event_memory);
+		};
+		
+		//	un-binds trigger event from the element
+		function removeEventHandler(element){
+			element.unbind(settings.triggered_by);
+		};
+		
+		//	re-binds a pre-existing trigger event to the element
+		function restoreEventHandler(element){
+			var events = element.data().__deferred_event_handlers__;
+			for (var event in events){
+				var event_type = events[event].type;
+				var event_handler = events[event].handler;
+				element.unbind(event_type);
+				element.bind(event_type, event_handler);
+				element[event_type]();
+				element.unbind(event_type);
+				bindConfirmHandler(element);
+			}
+		};
+		
+		//	initializes the confirm handler to the specified trigger events
+		function bindConfirmHandler(element){
+			var classname = element.attr('class');
+			var confirm_message = classname.substring(classname.indexOf("{")+1, classname.lastIndexOf("}")).split(":")[1]; // good candidate for a regexp
+			element.bind(settings.triggered_by, (function(){showConfirm(element, confirm_message)}));
+		};
+		
 		//	the handler that intercepts the action
 		function showConfirm(target_element, message){
 			var confirmed = window.confirm(message);
 			if (confirmed){
-				
-			} else {
+				restoreEventHandler(target_element);
 				return false;
+			} else {
+				return true;
 			}
-		};
-		
-		//	stashes existing bound events on the element
-		function rememberEventHandlers(element){
-			element.data('__deferred_event_handlers__', element.data("events"));
 		};
 	};
 })(jQuery);
