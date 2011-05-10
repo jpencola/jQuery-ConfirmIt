30cbea59e6727843d531bd5cd03abeb101e2cd77
jpencola <paulpencola@gmail.com>
2011-05-10 01:46:13 -0400

    Properly structured the function body for init to support jQuery chaining. Updated TODO list.

--- src/jquery.confirm.it.js
+++ src/jquery.confirm.it.js
@@ -1,20 +1,6 @@
 (function($){
 	var methods = {
 		init: function(options){
-			console.log('init');
-			//	default settings
-			var settings = {
-				triggered_by: "click",
-				message: "Are you sure?"
-			};
-	
-			//	apply any overrides to the settings
-			$.extend(settings, options);
-			
-			//	listen for future elements added to the DOM 
-			$('body').unbind('DOMNodeInserted.confirmit');
-			$('body').bind('DOMNodeInserted.confirmit', function(event){$(event.target).confirmIt('init')});
-			
 			//	for each matching element that needs a confirmation step...
 			//	iterate over all events that it has currently bound
 			//	find the events that match the triggered_by name
@@ -22,7 +8,18 @@
 			//	un-bind all of them
 			//	bind the confirm event so it is the first one in the bubbling phase
 			//	re-bind all the other events
-			$(this).each(function() {
+			return this.each(function(){
+				var settings = {
+					triggered_by: "click",
+					message: "Are you sure?"
+				};
+				//	apply any overrides to the settings
+				if (options) $.extend(settings, options);
+				
+				//	listen for future elements added to the DOM 
+				$('body').unbind('DOMNodeInserted.confirmit');
+				$('body').bind('DOMNodeInserted.confirmit', function(event){$(event.target).confirmIt('init', options)});
+				
 				var element = $(this);
 				var is_already_initialized = !!(element.data('data-confirmit-ready')); 
 				if (is_already_initialized) return;
@@ -42,69 +39,69 @@
 				} else {
 					bindConfirmHandler(element);
 				}
-			});
-			
-			//	stashes existing trigger events for re-use later
-			function rememberEventHandlers(element, event_memory){
-				var events = element.data('events');
-				for (var event in events){
-					for (var i=0, count=events[event].length; i<count; i++){
-						var event_object = events[event][i];
-						if (event_object.type == settings.triggered_by){
-							event_memory.push({
-								type: event_object.type,
-								handler: event_object.handler
-							});
+				
+				//	stashes existing trigger events for re-use later
+				function rememberEventHandlers(element, event_memory){
+					var events = element.data('events');
+					for (var event in events){
+						for (var i=0, count=events[event].length; i<count; i++){
+							var event_object = events[event][i];
+							if (event_object.type == settings.triggered_by){
+								event_memory.push({
+									type: event_object.type,
+									handler: event_object.handler
+								});
+							}
 						}
 					}
-				}
-				element.data('__deferred_event_handlers__', event_memory);
-			};
-			
-			//	un-binds trigger events from the element
-			function removeEventHandlers(element){
-				element.unbind(settings.triggered_by);
-			};
-			
-			//	re-binds pre-existing trigger events to the element
-			function restoreEventHandlers(element){
-				var events = element.data().__deferred_event_handlers__;
-				for (var event in events){
-					var event_type = events[event].type;
-					var event_handler = events[event].handler;
-					element.bind(event_type, event_handler);
-				}
-			};
+					element.data('__deferred_event_handlers__', event_memory);
+				};
+				
+				//	un-binds trigger events from the element
+				function removeEventHandlers(element){
+					element.unbind(settings.triggered_by);
+				};
+				
+				//	re-binds pre-existing trigger events to the element
+				function restoreEventHandlers(element){
+					var events = element.data().__deferred_event_handlers__;
+					for (var event in events){
+						var event_type = events[event].type;
+						var event_handler = events[event].handler;
+						element.bind(event_type, event_handler);
+					}
+				};
 
-			//	Extracts the confirmation message from the element 
-			//	HTML5 data attribute is prefferred...
-			//	then the class attribute...
-			//	finally, fall back on the default message in settings
-			function getConfirmMessage(element){
-				var confirm_message, classname = element.attr('class');
-				confirm_message = element.attr('data-confirmit-message');
-				if (!confirm_message) confirm_message = classname.substring(classname.indexOf("{")+1, classname.lastIndexOf("}")).split(":")[1]; // good candidate for a regexp
-				if (!confirm_message) confirm_message = settings.message;
-				return confirm_message;
-			};
-			
-			//	binds the confirm handler to the specified trigger event
-			function bindConfirmHandler(element){
-				element.data('data-confirmit-ready', true);
-				element.bind(settings.triggered_by + '.confirmit', (function(e){showConfirm(e, element, getConfirmMessage(element))}));
-			};
-			
-			//	Display a confirm dialog
-			//	If the user declines then first stop any further bubbling of the event
-			//	then, if the element is an Anchor, prevent it's default behavior.
-			function showConfirm(event, target_element, message){
-				var confirmed = window.confirm(message);
-				if (!confirmed){
-					event.stopImmediatePropagation();
-					var is_anchor_element = !!(event.target.nodeName === 'A');
-					if (is_anchor_element) event.preventDefault();
-				}
-			};
+				//	Extracts the confirmation message from the element 
+				//	HTML5 data attribute is prefferred...
+				//	then the class attribute...
+				//	finally, fall back on the default message in settings
+				function getConfirmMessage(element){
+					var confirm_message, classname = element.attr('class');
+					confirm_message = element.attr('data-confirmit-message');
+					if (!confirm_message) confirm_message = classname.substring(classname.indexOf("{")+1, classname.lastIndexOf("}")).split(":")[1]; // good candidate for a regexp
+					if (!confirm_message) confirm_message = settings.message;
+					return confirm_message;
+				};
+				
+				//	binds the confirm handler to the specified trigger event
+				function bindConfirmHandler(element){
+					element.data('data-confirmit-ready', true);
+					element.bind(settings.triggered_by + '.confirmit', (function(e){showConfirm(e, element, getConfirmMessage(element))}));
+				};
+				
+				//	Display a confirm dialog
+				//	If the user declines then first stop any further bubbling of the event
+				//	then, if the element is an Anchor, prevent it's default behavior.
+				function showConfirm(event, target_element, message){
+					var confirmed = window.confirm(message);
+					if (!confirmed){
+						event.stopImmediatePropagation();
+						var is_anchor_element = !!(event.target.nodeName === 'A');
+						if (is_anchor_element) event.preventDefault();
+					}
+				};
+			});
 		},
 		destroy: function(){
 			// TODO
