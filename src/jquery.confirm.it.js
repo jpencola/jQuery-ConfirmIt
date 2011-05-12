f0714eaf45e930c442798d8043c561f48227edfb
Steve Perrie <stuff@mindstorminteractive.com>
2011-05-12 10:48:11 -0400

    added leavePage warning to test.htm

--- src/jquery.confirm.it.js
+++ src/jquery.confirm.it.js
@@ -25,6 +25,7 @@
 				if (is_already_initialized) return;
 				
 				var events_data = element.data('events');
+				console.dir(events_data);
 				var hasEvents = !!events_data;
 				var hasTriggerEvent = (function(){try{return !!events_data[settings.triggered_by]}catch(ex){return false}})();
 				if (hasEvents && hasTriggerEvent){
@@ -105,6 +106,7 @@
 		},
 		destroy: function(){
 			// TODO
+			//should init store the matching elements so destroy can unbind them?
 		}
 	};
 	
