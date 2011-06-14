02176e2762b47b88935764612be1718d67e7b4d2
jpencola <paulpencola@gmail.com>
2011-06-14 01:50:36 -0400

    Beefed-up setup and teardown routines. Added a couple new tests.

--- tests/specs/ConfirmItSpec.js
+++ tests/specs/ConfirmItSpec.js
@@ -3,17 +3,30 @@ describe('ConfirmIt', function() {
 		var testbed;
 		var test_element;
 		
+		//	setup
 		beforeEach(function(){
 			testbed = $("#test1");
-			testbed.empty();
 			testbed.append("<div id='init-test'>Test</div>");
 			test_element = $("#init-test");
 			test_element.click(function(){alert("clicked me!")});
 		});
 		
+		//	tear-down
+		afterEach(function(){
+			test_element.confirmIt('destroy');
+			testbed.empty();
+			ConfirmIt.instances = [];
+			delete test_element;
+		})
+		
 		it('Should have "data-confirmit-ready" data attribute set to true.', function() {
 			test_element.confirmIt();
 			expect(test_element).toHaveData("data-confirmit-ready", true);
+		});
+		
+		it('Should have "data-confirmit-index" data attribute set to 0.', function() {
+			test_element.confirmIt();
+			expect(test_element).toHaveData("data-confirmit-index", 0);
 		});
 		
 		it('Should have the "DOMNodeInserted" Confirmit event bound when "live" is set.', function() {
@@ -24,10 +37,11 @@ describe('ConfirmIt', function() {
 		});
 	});
 	
-	describe('Post Destroy Suite', function() {
+	describe('Destroy Suite', function() {
 		var testbed;
 		var test_element;
 		
+		//	setup
 		beforeEach(function(){
 			testbed = $("#test2");
 			testbed.empty();
@@ -36,17 +50,30 @@ describe('ConfirmIt', function() {
 			test_element.click(function(){alert("clicked me!")});
 		});
 		
+		//	tear-down
+		afterEach(function(){
+			test_element.confirmIt('destroy');
+			testbed.empty();
+			ConfirmIt.instances = [];
+			delete test_element;
+		})
+		
 		it('Should not have "data-confirmit-ready" data attribute.', function() {
 			test_element.confirmIt();
 			test_element.confirmIt('destroy');
 			expect(test_element).not.toHaveData("data-confirmit-ready");
 		});
 		
+		it('Should not have "data-confirmit-index" data attribute.', function() {
+			test_element.confirmIt();
+			test_element.confirmIt('destroy');
+			expect(test_element).not.toHaveData("data-confirmit-index");
+		});
+		
 		it('Should not have the "triggered_by" Confirmit event bound.', function() {
 			test_element.confirmIt();
 			test_element.confirmIt('destroy');
 			var events = test_element.data('events');
-			console.log(events);
 			var event = events['click.confirmit'];
 			expect(event).not.toBeDefined();
 		});
